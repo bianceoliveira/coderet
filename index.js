5e066b68153d1157ced83721ce4747d1651cbbe0
Biance Oliveira <bianceoliveira@gmail.com>
2021-03-11 00:02:40 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var nome = "Biance"
+var idade = 18
+var pessoa = {
+    nome: "Biance",
+    idade: 18, 
+    brasileira: true,
+    altura: 1.65, 
+
+}
+
+console.log("hello world " + nome)
+
+if (idade >= 18){
+
+    console.log(`${pessoa.nome} é maior de idade porque tem ${idade} anos` )
+} else {
+    console.log(`${nome} não é maior de idade porque tem ${idade} anos`);
+}
